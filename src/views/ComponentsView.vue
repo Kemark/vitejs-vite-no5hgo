0d48103489af1bf7d9d262e48724e9f7980dffb7
null <null>
2024-06-07 14:12:59 +0200

    add primeflex

--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -5,6 +5,7 @@ import Accordion from 'primevue/accordion';
 import AccordionTab from 'primevue/accordiontab';
 import Calendar from 'primevue/calendar';
 import Slider from 'primevue/slider';
+import '/node_modules/primeflex/primeflex.css'
 
 const msg = ref('');
 const date = ref('');
@@ -64,30 +65,70 @@ const value = ref(10);
     </Accordion>
   </nav>
 
-    <h1>3. Date</h1>
+  <h1>3. Date</h1>
 
-    <h2>3.1. Standard HTML</h2>
-    <label for="date">Date: </label>
-    <input type="date" id="date">
+  <h2>3.1. Standard HTML</h2>
+  <label for="date">Date: </label>
+  <input type="date" id="date">
 
-    <h2>3.2. Primevue Calendar</h2>
-    <section>
-      <Calendar v-model="date" />
-    </section>
- 
-    <h1>4. Slider</h1>
-    
-    <h2>4.1 Standart HTML</h2>
-    <input type="range"/>
-
-    <h2>4.2 Primevue Slider</h2>
-    <section>
-      <Slider v-model="value" :step="20" />
-    </section>
-
-
+  <h2>3.2. Primevue Calendar</h2>
   <section>
+    <Calendar v-model="date" />
+  </section>
 
+  <h1>4. Slider</h1>
+
+  <h2>4.1 Standart HTML</h2>
+  <input type="range" />
+
+  <h2>4.2 Primevue Slider</h2>
+  <section>
+    <Slider v-model="value" :step="20" />
+  </section>
+
+  <h2>5. Primeflex</h2>
+  <section>
+    <div class="grid">
+      <div class="col-4">
+        <div class="text-center p-3 border-round-sm bg-primary font-bold ">4</div>
+      </div>
+      <div class="col">
+        <div class="text-center p-3 border-round-sm bg-primary font-bold ">1</div>
+      </div>
+      <div class="col">
+        <div class="text-center p-3 border-round-sm bg-primary font-bold ">1</div>
+      </div>
+      <div class="col">
+        <div class="text-center p-3 border-round-sm bg-primary font-bold ">1</div>
+      </div>
+      <div class="col">
+        <div class="text-center p-3 border-round-sm bg-primary font-bold ">1</div>
+      </div>
+      <div class="col">
+        <div class="text-center p-3 border-round-sm bg-primary font-bold ">1</div>
+      </div>
+      <div class="col">
+        <div class="text-center p-3 border-round-sm bg-primary font-bold ">1</div>
+      </div>
+      <div class="col">
+        <div class="text-center p-3 border-round-sm bg-primary font-bold ">1</div>
+      </div>
+      <div class="col">
+        <div class="text-center p-3 border-round-sm bg-primary font-bold ">1</div>
+      </div>
+    </div>
+
+    <div class="grid">
+      <div class="col-2">
+        <div class="text-center p-3 border-round-sm bg-primary font-bold ">2</div>
+      </div>
+      <div class="col-6">
+        <div class="text-center p-3 border-round-sm bg-primary font-bold ">6</div>
+      </div>
+      <div class="col-4">
+        <div class="text-center p-3 border-round-sm bg-primary font-bold ">4</div>
+      </div>
+    </div>
   </section>
 </template>
 
